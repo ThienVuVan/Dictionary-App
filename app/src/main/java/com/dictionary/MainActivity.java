29c3ae9906ef64d7d4ebef133361a87f92c36402
hieuhoang26 <hhh262434@gmail.com>
2024-03-15 15:39:03 +0700

    text_trans

--- app/src/main/java/com/dictionary/MainActivity.java
+++ app/src/main/java/com/dictionary/MainActivity.java
@@ -1,13 +1,32 @@
 package com.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Loại bỏ tiểu đề mặc định
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.white));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"ádđ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
